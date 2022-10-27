287195ce8e77b17f90298318e6f48c23fe8d2b16
Senne <>
2022-10-27 16:06:53 +0200

    feat: add CORS to App.JS

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/messages', messagesRouter);
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
